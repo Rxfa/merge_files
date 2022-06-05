7f3cb470d2643a618f20137460defecf8a69034c
Rxfa <65974055+Rxfa@users.noreply.github.com>
2022-06-05 19:07:43 +0100

    Uploaded the script

---
+++ merge_file.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# github.com/rxfa
+
+from colorama import Fore
+import sys
+
+app_name = sys.argv[0]
+
+def main():
+    fname_out = input("What will the name of the output file be?")
+    try: 
+        open(fname_out, 'x')
+    except:
+        print(f'{fname_out}')
+
+    def merge(file):
+        with open(file, 'r+', encoding='utf8', errors='ignore') as file:
+            fout = open(fname_out, 'r+', encoding='utf8', errors='ignore')
+            for line in file:
+                if line not in fout:
+                    fout.write(line)
+            fout.close()
+        return (Fore.GREEN + f"[+]{file} merged successfully!")
+
+    n = 1
+    while n <= (len(sys.argv)-1):
+        merge(sys.argv[n])
+        n += 1
+
+
+if __name__ == '__main__':
+    if (len(sys.argv)-1) <= 1:
+        print(f'[-] {app_name} requires at least 2 two files!')
+        print(f'[-] Usage: {app_name} <file1> <file2> ...')
+    else:
+        main()
