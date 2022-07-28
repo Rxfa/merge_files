a7481bb69b4cde9405cfc352cfd2f53d3e182071
Rafael N <65974055+Rxfa@users.noreply.github.com>
2022-07-28 13:49:53 +0100

    Improved code readability

--- fms.py
+++ fms.py
@@ -2,33 +2,34 @@
 
 import sys
 
-app_name = sys.argv[0]
 
 def main():
     fname_out = input("What will the name of the output file be?")
-    try: 
-        open(fname_out, 'x')
+    try:
+        open(fname_out, "x")
     except:
-        print(f'{fname_out}')
+        print(f"{fname_out}")
 
     def merge(file):
-        with open(file, 'r+', encoding='utf8', errors='ignore') as file:
-            fout = open(fname_out, 'r+', encoding='utf8', errors='ignore')
+        with open(file, "r+", encoding="utf8", errors="ignore") as file:
+            fout = open(fname_out, "r+", encoding="utf8", errors="ignore")
             for line in file:
                 if line not in fout:
                     fout.write(line)
             fout.close()
-        return (f"[+]{file} merged successfully!")
+        return f"[+]{file} merged successfully!"
 
-    n = 1
-    while n <= (len(sys.argv)-1):
-        merge(sys.argv[n])
-        n += 1
+    def iterate():
+        n = list(x for x in range(1, len(sys.argv)))
+        iteration = map(lambda x: merge(sys.argv[x]), n)
+        return iteration
+
+    iterate()
 
 
-if __name__ == '__main__':
-    if (len(sys.argv)-1) <= 1:
-        print(f'[-] {app_name} requires at least 2 two files!')
-        print(f'[-] Usage: {app_name} <file1> <file2> ...')
+if __name__ == "__main__":
+    if len(sys.argv) in range(3):
+        print(f"[-] {sys.argv[0]} requires at least 2 two files!")
+        print(f"[-] Usage: {sys.argv[0]} <file1> <file2> ...")
     else:
         main()
